02b0dd1f8da1b098f9ae325567432026db5c16e4
Yong Su <ysu@box.com>
2016-06-17 12:04:01 -0700

    Scaffolding.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// node
+// vendors
+import React from 'react';
+// project
+import Menu from './components/Menu';
+
+const App = () => (
+  <div className="mainApp">
+    <Menu />
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+export default App;
