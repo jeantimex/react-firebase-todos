4200dc3240a48a6dc297b4b5131f63ab67aafd10
jeantimex <jean.timex@gmail.com>
2016-06-17 13:30:31 -0700

    Added email password auth and push one todo once logged in.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import firebase from 'firebase';
 
 import 'app.scss';
 
@@ -9,6 +10,32 @@ class App extends Component {
 
     constructor(props) {
         super(props);
+    }
+
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyB3RPtTjHF_8JmPnZdEd69B91-E9jtRor0',
+            authDomain: 'todos-aefcd.firebaseapp.com',
+            databaseURL: 'https://todos-aefcd.firebaseio.com',
+            storageBucket: '',
+        };
+
+        const app = firebase.initializeApp(config);
+
+        const email = 'jean.timex@gmail.com';
+        const password = '1234566';
+
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+            const ref = firebase.database().ref();
+            const todosRef = ref.child('todos');
+            todosRef.push({
+                id: '1',
+                text: 'Hello World',
+            });
+        }, function (error) {
+            console.error(error);
+        });
     }
 
     render() {
