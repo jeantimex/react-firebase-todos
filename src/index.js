8633c0bd0e2d28774fb5bf1d96bd71be53331285
jeantimex <jean.timex@gmail.com>
2016-06-17 17:09:11 -0700

    Implemeted the todo app based on http://redux.js.org/docs/basics/ExampleTodoList.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import App from './components/App';
 
-import App from './App';
+let store = createStore(todoApp);
 
-render(<App />, document.getElementById('container'));
+render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
